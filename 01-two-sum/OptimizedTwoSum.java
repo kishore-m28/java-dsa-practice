35a43cfb97b1351891749a4d9ae034a4d81821e2
kishore-m28 <164298250+kishore-m28@users.noreply.github.com>
2025-05-16 22:23:27 +0530

    Add better solution for Two Sum using HashMap

---
+++ 01-two-sum/OptimizedTwoSum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int arr[] = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            Integer value = map.get(target - nums[i]);
+            if(value == null){
+                map.put(nums[i], i);
+            }else{
+                arr[0]=value;
+                arr[1] = i;
+                break;
+            }
+        }
+        return arr;
+    }
+}
